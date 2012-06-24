1d22ed43c9de2fd7717e92c415aefc01a8c94a9a
Rob Terhaar <rob@atlanticdynamic.com>
2012-06-23 23:31:49 -0400

    api complete, but blocking on the progress bar due to unfinished_tasks

--- starcluster/threadpool.py
+++ starcluster/threadpool.py
@@ -3,9 +3,10 @@ ThreadPool module for StarCluster based on WorkerPool
 """
 import time
 import Queue
+#import workerpool
 from gevent import pool
 
-from starcluster import exception
+#from starcluster import exception
 from starcluster import progressbar
 from starcluster.logger import log
 
@@ -13,42 +14,100 @@ from starcluster.logger import log
 class AintJava(Exception):
     pass
 
+class ThreadPool(object):
+    def __init__(self, size=1, disable_threads=False, job_timeout=600):
+        self.disable_threads = disable_threads
+        self.results_queue = Queue.Queue()
+        self._progress_bar = None
+        if self.disable_threads: size = 0
+        self.pool = pool.Pool(size)
+        self.job_timeout = job_timeout
 
-class SimpleJob(object):
-    def __init__(self, method, args=None, kwargs=None, jobid=None,
-                 results_queue=None, thread_timeout=240, job_timeout=600):
-        """ method : a single function call to the job that should be run
-            args : any args for the function, as a list
-            kwargs : any optional kwargs, as a dict
-            jobid : a way to find the job, currently not required for gevent
-            results_queue : status will be available here when jobs finish
-            thread_timeout : timeout(secs) for a single thread
-            job_timeout : once all jobs have been submitted and running,
-                          python will block for N seconds before killing
-                          any background threads
-        """
+    @property
+    def progress_bar(self):
+        if not self._progress_bar:
+            widgets = ['', progressbar.Fraction(), ' ',
+                       progressbar.Bar(marker=progressbar.RotatingMarker()),
+                       ' ', progressbar.Percentage(), ' ', ' ']
+            pbar = progressbar.ProgressBar(widgets=widgets, maxval=1,
+                                           force_update=True)
+            self._progress_bar = pbar
+        return self._progress_bar
+
+    def simple_job(self, method, args=None, kwargs=None, jobid=None):
+
         if args is None:
             args = []
 
         if kwargs is None:
             kwargs = {}
 
-        self.method = method
-        self.args = args
-        self.kwargs = kwargs
-        self.jobid = jobid
-        self.results_queue = results_queue
-        self.pool = pool.Pool(thread_timeout)
-        self.job_timeout = job_timeout
+        if isinstance(args, list) or isinstance(args, tuple):
+            if isinstance(kwargs, dict):
+                r = self.pool.spawn(method, *args, **kwargs)
+            else:
+                r = self.pool.spawn(method, *args)
+        elif args is not None and args is not []:
+            if isinstance(kwargs, dict):
+                r = self.pool.spawn(method, args, **kwargs)
+            else:
+                r = self.pool.spawn(method, args)
+        else:
+            r = self.pool.spawn(method)
 
-    def run(self):
-        self.results_queue.append(self.pool.spawn(self.method,
-                                                  *self.args,
-                                                  **self.kwargs))
+        if self.results_queue:
+            return self.results_queue.put(r)
+        return r
+
+
+    def get_results(self):
+        results = []
+        for i in range(self.results_queue.qsize()):
+            results.append(self.results_queue.get().value())
+        return results
+
+    def map(self, fn, *seq):
+        self.pool.map(fn, *seq)
+
+    def store_exception(self, e):
+        self._exception_queue.put(e)
+
+    def shutdown(self):
+        log.info("Shutting down threads...")
+        self.pool.kill(timeout=self.job_timeout)
+
+    @property
+    def unfinished_tasks(self):
+        return self.results_queue.qsize()
+
+    def wait(self, numtasks=None, return_results=True):
+        pbar = self.progress_bar.reset()
+        pbar.maxval = self.unfinished_tasks
+        if numtasks is not None:
+            pbar.maxval = max(numtasks, self.unfinished_tasks)
+        while self.unfinished_tasks != 0:
+            finished = pbar.maxval - self.unfinished_tasks
+            pbar.update(finished)
+            log.debug("unfinished_tasks = %d" % self.unfinished_tasks)
+            time.sleep(1)
+        if pbar.maxval != 0:
+            pbar.finish()
+        self.pool.join(timeout=self.job_timeout)
+#        if self._exception_queue.qsize() > 0:
+#            raise exception.ThreadPoolException(
+#                "An error occurred in ThreadPool", self._exception_queue.queue)
+        if return_results:
+            return self.get_results()
 
     def join(self):
-        self.pool.gevent.joinall(self.results_queue, self.job_timeout)
+        self.pool.join(timeout=self.job_timeout)
+#        self.pool.joinall(self.results_queue, self.job_timeout)
 
-def get_thread_pool(*args, **kwargs):
-    msg = 'Please convert this object call from get_thread_pool to ThreadPool'
-    raise AintJava(msg)
+    def __del__(self):
+        log.debug('del called in threadpool')
+        self.shutdown()
+        self.join()
+
+def get_thread_pool(size=10, worker_factory=None,
+                    disable_threads=False):
+    return SimpleJob(size=size, disable_threads=disable_threads)
